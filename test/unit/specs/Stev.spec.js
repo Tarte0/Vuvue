08aa05e35439dde7cd07a751d84173aa1a20993c
Steeve Ciminera <ciminera.steeve@gmail.com>
2018-11-07 14:12:30 +0100

    Initial commit

---
+++ test/unit/specs/Stev.spec.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Stev from '@/components/Stev'
+
+describe('HelloWorld.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(Stev)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('.msgDiv h1').textContent)
+      .toEqual('Tuez moi')
+  })
+})
